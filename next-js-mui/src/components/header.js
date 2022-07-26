6114f04afd627e6be6e2be340887226eaf60ae55
couragenki <genki.meado@gmail.com>
2022-07-26 13:48:11 +0900

    add components

---
+++ next-js-mui/src/components/header.js
@@ -0,0 +1,15 @@
+const Header = () => {
+    return (
+      <header>
+        <div>ヘッダー</div>
+        <style jsx>{`
+            header {
+                height: 50px;
+                background: #F0F0F0;
+            }
+        `}</style>
+      </header>
+    );
+}
+
+export default Header;
---
+++ next-js-mui/src/pages/index.js
@@ -0,0 +1,27 @@
+import Header from '../components/header'
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Header />
+
+      <style jsx>{`
+      `}</style>
+
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+            sans-serif;
+        }
+
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
+    </div>
+  )
+}
